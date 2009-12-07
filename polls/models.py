a1a74b0a489f1fb85dff2e626f515f3015b4e509
unknown <D:\Personal\Mail>
2009-12-07 11:19:04 +0300

    Init django project

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from django.db import models
+
+# Create your models here.
+class Poll(models.Model):
+  """
+  Модель голосования
+  """
+  question = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('Дата публикации')
+
+  def __unicode__(self):
+    return self.question
+
+  def was_published_today(self):
+    """Голосование опубликовано сегодня?"""
+    return self.pub_date.date() == datetime.date.today()
+
+
+class Choice(models.Model):
+  """
+  Модель выбора голосования
+  """
+  poll = models.ForeignKey(Poll)
+  choice = models.CharField(max_length=200)
+  votes = models.IntegerField()
+
+  def __unicode__(self):
+    return self.choice
+
+
